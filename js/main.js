f34a0a2dddb5d2aabd33813dfce9c5c96f520989
reemmohamed8433@gmail.com <reemmohamed8433@gmail.com>
2025-06-17 23:54:06 +0300

    update

--- js/main.js
+++ js/main.js
@@ -213,7 +213,7 @@ document.addEventListener('DOMContentLoaded', function () {
         reply_to: email,
         phone: phone,
         message: message,
-        file_url: driveFileUrl,
+        file_url: driveFileUrl || "No file uploaded",
         to_email: "Deebco99@gmail.com"
       });
 
