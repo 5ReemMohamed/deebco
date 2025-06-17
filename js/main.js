0134b2afb7b22afd4a901a6544af80c0ba37a639
reemmohamed8433@gmail.com <reemmohamed8433@gmail.com>
2025-06-17 23:21:05 +0300

    update

--- js/main.js
+++ js/main.js
@@ -203,11 +203,11 @@ if (pdfFile) {
     // EmailJS Send
     try {
     emailjs.send('service_b49n80l', 'template_2zsbqir', {
-        from_name: nameInput.value,
-        reply_to: emailInput.value,
-        phone: phoneInput.value,
-        message: messageInput.value,
-        file_url: driveFileUrl,
+        from_name: name,             
+        reply_to: email,             
+        phone: phone,                
+        message: message,            
+        file_url: driveFileUrl,      
         to_name: "Deebco",
         to_email: "Deebco99@gmail.com"
 
